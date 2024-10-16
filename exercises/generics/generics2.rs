cb10f72ab4c7a0f9dd198a62ee0c5f24d31cdb91
piggy <162404462@qq.com>
2024-10-16 10:49:48 +0800

    58

--- exercises/generics/generics2.rs
+++ exercises/generics/generics2.rs
@@ -6,29 +6,8 @@
 // Execute `rustlings hint generics2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-struct Wrapper {
-    value: u32,
-}
-
-impl Wrapper {
-    pub fn new(value: u32) -> Self {
-        Wrapper { value }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn store_u32_in_wrapper() {
-        assert_eq!(Wrapper::new(42).value, 42);
-    }
-
-    #[test]
-    fn store_str_in_wrapper() {
-        assert_eq!(Wrapper::new("Foo").value, "Foo");
-    }
+fn main() {
+    let mut shopping_list: Vec<&str> = Vec::new();
+    shopping_list.push("milk");
 }
